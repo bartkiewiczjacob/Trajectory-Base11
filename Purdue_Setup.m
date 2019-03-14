1cfd919b6dfb165843332efc04088c46cb099440
Jacob Phillip Bartkiewicz <jbartkie@purdue.edu>
2019-03-13 20:14:01 -0500

    Changed Purdue_Setup to assist RCS team in determining ops

--- Purdue_Setup.m
+++ Purdue_Setup.m
@@ -12,22 +12,35 @@ m_dot = -20; %[lbs/sec]
 m_dot_slugs = m_dot / 32.2; %[slugs/sec]
 diameter = 18; %[in]
 diameter_ft = diameter / 12; %[ft]
-length = 24; %[ft]
+rocket_length = 24; %[ft]
 burn_time = 200000/Thrust; %[sec]
 wet_mass = 1200; %[lbs]
 dry_mass = wet_mass + m_dot * burn_time; %[lbs]
 wet_mass_slugs = wet_mass / 32.2; %[slugs]
 dry_mass_slugs = dry_mass / 32.2; %[slugs]
 I_wet = 1/12 * wet_mass_slugs * [3*(diameter_ft/2)^2, 0, 0; ...
-                           0, length^2 + 3*(diameter_ft/2)^2, 0; ...
-                           0, 0, length^2 + 3*(diameter_ft/2)^2]; %[wet mass Inertia Tensor]
+                           0, rocket_length^2 + 3*(diameter_ft/2)^2, 0; ...
+                           0, 0, rocket_length^2 + 3*(diameter_ft/2)^2]; %[wet mass Inertia Tensor]
 I_dry = 1/12 * dry_mass_slugs * [3*(diameter_ft/2)^2, 0, 0; ...
-                           0, length^2 + 3*(diameter_ft/2)^2, 0; ...
-                           0, 0, length^2 + 3*(diameter_ft/2)^2]; %[dry mass Inertia Tensor]
-%sim('Purdue_Sim')
-%plot3(trajectory(:,1), trajectory(:,2), trajectory(:,3));
-%grid on;
-%view(30,30);
-%burn = trajectory - drift;
+                           0, rocket_length^2 + 3*(diameter_ft/2)^2, 0; ...
+                           0, 0, rocket_length^2 + 3*(diameter_ft/2)^2]; %[dry mass Inertia Tensor]
+sim('Purdue_Sim');
+j = 1;
+Mach_op = [];
+density_op = [];
+for i = 1:1373
+    for k = 1:length(Mach_op)
+        if abs(Mach(i) - Mach_op(k)) < 0.001
+            op(j) = time(i);
+            j = j + 1;
+        end
+    end
+    for x = 1:length(density_op)
+        if abs(density(i) - density_op(x)) < 0.001
+            op(j) = time(i);
+            j = j + 1;
+        end
+    end
+end
 
 
