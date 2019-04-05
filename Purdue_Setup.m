dc190fa8e678f151c344ba95917504c08e4aff86
Jacob Phillip Bartkiewicz <jbartkie@purdue.edu>
2019-04-05 17:26:30 -0400

    Preparation for tuning

--- Purdue_Setup.m
+++ Purdue_Setup.m
@@ -14,7 +14,7 @@ diameter = 18; %[in]
 diameter_ft = diameter / 12; %[ft]
 rocket_length = 24; %[ft]
 burn_time = 200000/Thrust; %[sec]
-wet_mass = 1200; %[lbs]
+wet_mass = 1300; %[lbs]
 dry_mass = wet_mass + m_dot * burn_time; %[lbs]
 wet_mass_slugs = wet_mass / 32.2; %[slugs]
 dry_mass_slugs = dry_mass / 32.2; %[slugs]
@@ -24,32 +24,9 @@ I_wet = 1/12 * wet_mass_slugs * [3*(diameter_ft/2)^2, 0, 0; ...
 I_dry = 1/12 * dry_mass_slugs * [3*(diameter_ft/2)^2, 0, 0; ...
                            0, rocket_length^2 + 3*(diameter_ft/2)^2, 0; ...
                            0, 0, rocket_length^2 + 3*(diameter_ft/2)^2]; %[dry mass Inertia Tensor]
-sim('Purdue_Sim');
-j = 1;
-Mach_op = [0.3 0.8 1.2 3.4];
-density_op = [0.002377 0.002377/5 0.002377/10 0.002377/50 0.002377/100];
-altitude_op = [];
-n = 1;
-m = 1;
-for i = 1:1357
-    for k = 1:length(Mach_op)
-        if abs(Mach(i) - Mach_op(k)) < 0.0022
-            mach_time_op(j) = time(i);
-            j = j + 1;
-        end
-    end
-    for x = 1:length(density_op)
-        if abs(density(i) - density_op(x)) < 0.0000001
-            density_time_op(n) = time(i);
-            n = n + 1;
-        end
-    end
-    for y = 1:length(altitude_op)
-        if abs(altitude(i) - altitude_op(y)) < 10
-            op(m) = time(i);
-            m = m + 1;
-        end
-    end
-end
+
+
+
+
 
 
