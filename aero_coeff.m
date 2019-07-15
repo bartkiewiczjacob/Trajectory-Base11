d2243253f7004e7b354d44cf186ce29a8395cf7a
arms31063-user <arms31063-user@arms31063pc2.ecn.purdue.edu>
2019-07-15 19:35:29 -0400

    pitch control software

--- aero_coeff.m
+++ aero_coeff.m
@@ -1,13 +1,24 @@
 function [CD, CL, CP] = aero_coeff(Ma, alpha)
-global Ma_val CD_data_mach CL_data_mach CP_data_mach CD_fit_alpha CL_fit_alpha CP_fit_alpha
+% compute the drag and lift coefficients and the center of pressure for
+% given Mach number and angle of attack
 
-index = find(Ma == Ma_val);
+% access aerodynamics test data
+global Ma_val CD_data_mach CL_data_mach CP_data_mach CD_fit_alpha CL_fit_alpha CP_fit_alpha time
+
+% find the coefficients from Ma (alpha = 0)
+index = find(abs(Ma_val-Ma) <= 1e-10);
 CD_0 = CD_data_mach(index);
 CL_0 = CL_data_mach(index);
 CP_0 = CP_data_mach(index);
 
-[val, index] = min(abs([0.1, 0.5, 1.1, 2, 5]-alpha));
+[val, index] = min(abs([0.1, 0.5, 1.1, 2, 5]-Ma)); % check which Ma case is closest
+% interpolate from the alpha = 0 value using the polyfit with closest Ma
 CD = CD_fit_alpha(1, index)*alpha^2 + CD_fit_alpha(2, index)*alpha + CD_0;
 CL = CL_fit_alpha(1, index)*alpha + CL_0;
-CP = CP_fit_alpha(1, index)*alpha^4 + CP_fit_alpha(2, index)*alpha^3 + CP_fit_alpha(3, index)*alpha^2 + CP_fit_alpha(4, index)*alpha + CP_0;
+CP = CP_fit_alpha(1, index)*alpha^4 + CP_fit_alpha(2, index)*alpha^3 + ...
+    CP_fit_alpha(3, index)*alpha^2 + CP_fit_alpha(4, index)*alpha + CP_0;
+
+% if time == 25.5
+%     keyboard
+% end
 end
--- pitch_sim_eqns.m
+++ pitch_sim_eqns.m
@@ -1,16 +1,26 @@
 function [xdot] = pitch_sim_eqns(t,x,w)
+% This function is called s-function pitch_sim_mdl (Simulink block
+% written in code).
+% This function is mainly the same as the main script, but it is in the
+% form xdot = f(t,x,w), with:
+% t = time
+% x = state vector
+% w = control input
+% This is necessary in order to get the right state space matrices when we
+% linearize
 
-global T D L G V m J h cp cu ts
+global T D L G V m J cp cu ts % time varying parameters
 
-phi = x(1);
-theta = x(2);
+%inputs
+theta = x(1);
+phi = x(2);
 U = w;
 
 B2E = [cos(pi/2+theta) 0 sin(pi/2+theta); 0 1 0; -sin(pi/2+theta) 0 cos(pi/2+theta)];
 V2E = [cos(pi/2+phi) 0 sin(pi/2+phi); 0 1 0; -sin(pi/2+phi) 0 cos(pi/2+phi)];
 
 att = B2E*[1; 0; 0];
-v = V*V2E*[1; 0; 0]; 
+v = V*V2E*[1; 0; 0];
 v_unit = v/V;
 v_perp = [-v(3); 0; v(1)];
 
@@ -22,14 +32,13 @@ u = B2E*[0; 0; U];
 
 f = t+l+d+g+u;
 a = f/m;
-h = a(3)*ts^2/2 + v(3)*ts + h;
-
 v = v + a*ts;
-V = norm(v);
-phi_dot = (atan(v(3)/v(1)) - phi)/ts; % phi_dot = cos(phi)^2*[a(1)*v(3)-a(3)*v(1)]/v(3)^2;
+
+phi_dot = (atan(v(1)/v(3)) - phi)/ts; % phi_dot = cos(phi)^2*[a(1)*v(3)-a(3)*v(1)]/v(3)^2;
 theta_dot = 1/J(2,2) * (cross(l,cp) + cross(d,cp) + cross(u,cu));
 theta_dot = theta_dot(2);
 
+% outputs
 xdot(1) = theta_dot;
 xdot(2) = phi_dot;
 
