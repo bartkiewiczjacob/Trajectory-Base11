511eca5e62e4560be0c22fbc208f9b64accd3128
arthurdw <arthur.dewaleffe@gmail.com>
2019-09-18 10:46:20 -0400

    vertical launch function

--- vertical_launch.m
+++ vertical_launch.m
@@ -1,18 +1,22 @@
-function [dhdt] = vertical_launch(t, h, thrust, wmass, mdot)
+function [dhdt] = vertical_launch(t, h, thrust, wmass, mdot, burn_time)
 
-T = (thrust)*4.44822; % N
+if t < burn_time
+    T = (thrust)*4.44822; % N
+else
+    T = 0;
+end
 m = (wmass - mdot*t)*0.453592; % kg
 
 Cd = 0.3;
-d = 18*2.54; % cm
-S = pi*d^2/4; % cm2
+d = 18*0.0254; % m
+S = pi*d^2/4; % m2
 G = m*9.81; % N
 
-[T, A, P, RHO] = atmosisa(h(:,1));
+[temp, A, P, RHO] = atmosisa(h(1));
 
 dhdt = zeros(2,1);
-dhdt(:,1) = h(:,2);
-dhdt(:,2) = 1/m*(-Cd*0.5*transpose(RHO).*h(:,2)*S + T - G);
+dhdt(1) = h(2);
+dhdt(2) = 1/m*(-Cd*0.5*(RHO)*h(2)*S + T - G);
 
 
 
