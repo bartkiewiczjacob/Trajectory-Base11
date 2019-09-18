05e33a300ef197f2788edf32938056a2702a1b1f
arthurdw <arthur.dewaleffe@gmail.com>
2019-09-18 10:21:14 -0400

    vertical launch function

--- vertical_launch.m
+++ vertical_launch.m
@@ -8,11 +8,11 @@ d = 18*2.54; % cm
 S = pi*d^2/4; % cm2
 G = m*9.81; % N
 
-[T, A, P, RHO] = atmosisa(h(1));
+[T, A, P, RHO] = atmosisa(h(:,1));
 
 dhdt = zeros(2,1);
-dhdt(1) = h(2);
-dhdt(2) = 1/m*(-Cd*0.5*RHO*h(2)*S + T - G);
+dhdt(:,1) = h(:,2);
+dhdt(:,2) = 1/m*(-Cd*0.5*transpose(RHO).*h(:,2)*S + T - G);
 
 
 
