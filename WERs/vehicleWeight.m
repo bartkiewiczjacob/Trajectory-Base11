41e4c2bbddd33548ef2139a38c43d64b1e4cd196
ngurgens <ngurgens@purdue.edu>
2019-10-20 23:11:15 -0400

    Update vehicleWeight.m

--- WERs/vehicleWeight.m
+++ WERs/vehicleWeight.m
@@ -1,6 +1,10 @@
-function [weight] = vehicleWeight(diameter, height)
+function [mass] = vehicleWeight(diameter, height)
+
+    % height and diameter in inches
+    % calculates hollow cylinder mass
+
     density = .1;    %lbs/in^3
     wallThickness = .125; %in
     volume = pi() * ((diameter / 2)^2 - (diameter/2 - wallThickness)^2) * height;
-    weight = volume * density;
+    mass = volume * density;
 end
