699173f46532bc23380fba55e10c77b76a1e367e
ngurgens <ngurgens@purdue.edu>
2019-10-31 16:33:08 -0400

    Volume consideration modifications

--- WERs/Vehicle_WER.m
+++ WERs/Vehicle_WER.m
@@ -9,25 +9,29 @@
 % (limited by available stock) and the weight of the shell. 
 % Material is 0 for aluminum or 1 for fiberglass
 % All units in inches and lbs
-function [diameter, weight] = Vehicle_WER(D, L, material)
+function [diameter, weight, L] = Vehicle_WER(D, material)
+
     existing = linspace(4,10,25);   %Readily available sizes from https://www.vitaneedle.com/aluminum-tube/
 
     density = .1;    %lbs/in^3
     wallThickness = .125; %in
+    V = 2937.6; %in^3  Calculated volume of upper part of rocket to be used to find height
     
     if material == 1
         existing = [4.5 5.15 5.375 6.17 7.518 8.005];
         ID = [4.375 5 5.525 6 7.708 7.815];
         linearDensity = [16.8 19 16.8 24 25 27]./16./12; %Converting from oz/ft to lb/in
     end
-    [M,I] = min(abs(existing - D));
+    [~,I] = min(abs(existing - D));
     diameter = existing(I);
+    L = V ./ (pi * ((D-2*wallThickness) /2) .^ 2);     %Calulates the length of the upper section of the rocket
     if material == 1
 %         wallThickness = (diameter - ID(I))/2;
         weight = linearDensity(I) * L;
-        return;
+%        return;
     end
     
     volume = pi() * ((diameter / 2)^2 - (diameter/2 - wallThickness)^2) * L;
     weight = volume * density;
+     
 end
